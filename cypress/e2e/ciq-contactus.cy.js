b238d6aba20c8cd30acda1597d11019bd5e7570c
khushi-9bit <khushi.ojha@ninebit.in>
2025-07-19 11:01:01 +0530

    Update Contact us test case

--- cypress/e2e/ciq-contactus.cy.js
+++ cypress/e2e/ciq-contactus.cy.js
@@ -3,7 +3,7 @@ it("should successfully submit the Contact Us form", () => {
 
   cy.contains("We’d love to hear from you").should("be.visible");
   cy.get('input[name="customerName"]').type("Hrdesk Ninebit");
-  cy.get('input[aria-label="Organization name"]').type("NineBit Computing");
+  cy.get('input[name ="customerOrg"]').type("NineBit Computing");
   cy.get('input[name="customerEmail"]').type("hrdesk@ninebit.in");
   cy.get('input[name="customerPhone"]').type("1234567890");
   cy.get('textarea[name="customerMsg"]').type("Hi team, I would like to know more about CIQ's automation features.");
